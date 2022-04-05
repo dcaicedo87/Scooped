633d11c7f579b3462f1ec19c0a418d55e7a3d6f0
Junki Sato <j9sato11@gmail.com>
2022-04-04 22:03:16 -0600

    created few more ice creams to seed

--- app/seeds/icecreams.py
+++ app/seeds/icecreams.py
@@ -69,22 +69,22 @@ def seed_icecreams():
     icecream9 = IceCream(
         flavor_name="Bananas Foster​",
         category="Vegan",
-        icecream_pic_url="https://simpleveganblog.com/wp-content/uploads/2021/07/banana-ice-cream-2.jpg",
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi accusamus rerum molestias aliquid, laboriosam, explicabo magni repellat facere sequi blanditiis unde similique tenetur ab esse minus quam ratione veritatis quas earum nisi excepturi? Dolorem sapiente similique aliquid nisi voluptatibus, quas recusandae tenetur obcaecati, veritatis quo doloribus, blanditiis debitis natus! Ut?",
+        icecream_pic_url="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Bananas-Foster-Sundaes_EXPS_CIMZ17_42034_B07_13_4b.jpg",
+        description="Banana & Cinnamon Non-Dairy Frozen Dessert with Almond Toffee Pieces & a Salted Caramel Core",
         user_id=9,
     )
 
     icecream10 = IceCream(
         flavor_name="Non-Dairy Mint Chocochunk",
-        category="Dairy",
+        category="Vegan",
         icecream_pic_url="https://www.benjerry.com/files/live/sites/systemsite/files/flavors/products/us/pint/bananas-foster-core-sunflower-detail.png",
-        description="Banana & Cinnamon Non-Dairy Frozen Dessert with Almond Toffee Pieces & a Salted Caramel Core",
+        description="Sweet & smooth Mint flavored Frozen Dessert with Chocolate Chunks & gooey Swirls of Fudge.",
         user_id=10,
     )
 
     icecream11 = IceCream(
         flavor_name="P.B. & Cookies",
-        category="Dairy",
+        category="Vegan",
         icecream_pic_url="https://www.hersheyicecream.com/products/images/premium-gold/peanut-butter-cookies-and-cream-scoop.gif",
         description="Vanilla Non-Dairy Frozen Dessert with Chocolate Sandwich Cookies & Crunchy Peanut Butter Swirls",
         user_id=1,
@@ -131,11 +131,35 @@ def seed_icecreams():
     )
 
     icecream17 = IceCream(
-        flavor_name="Lychee Raspberry Daiquiri",
+        flavor_name="Rare Cheese Cake Ice Cream",
         category="Dairy",
-        icecream_pic_url="https://images.squarespace-cdn.com/content/v1/60874d1bdb18651f658460e2/1624988810968-M45TBXUPSC5XLKHW21IB/CC_0421_TONED_HR_026.jpg?format=750w",
-        description="We blend juicy lychees with coconut milk to make a sherbet that is both refreshing and creamy.",
-        user_id=6,
+        icecream_pic_url="https://image.entabe.jp/upload/images/sasa%281%29.jpg",
+        description="It tastes exactly what the name says - each mouthful tastes like a little piece a cheesecake in your mouth, with a slight hint of lemon and sweet honey. Then you can taste the biscuit base with more lemon taste.",
+        user_id=7,
+    )
+
+    icecream18 = IceCream(
+        flavor_name="Black Sesame",
+        category="Vegan",
+        icecream_pic_url="https://www.tablefortwoblog.com/wp-content/uploads/2021/06/black-sesame-ice-cream-recipe-photo-tablefortwoblog-5-scaled.jpg",
+        description="Black Sesame Ice cream is a deep, nutty and delicious good time! ",
+        user_id=8,
+    )
+
+    icecream19 = IceCream(
+        flavor_name="Peppermint Bark Gelato",
+        category="Gelato",
+        icecream_pic_url="https://food.fnr.sndimg.com/content/dam/images/food/plus/fullset/2020/07/27/0/FNP_Vallery_No-Churn-Peppermint-Bark-Ice-Cream-6_s4x3.jpg.rend.hgtvcom.616.462.suffix/1595882478738.jpeg",
+        description="We took white chocolate peppermint gelato and mixed in semi-sweet chocolatey flakes to create our Peppermint Bark Gelato, the perfect treat that tastes like winter in a pint. This flavor is only available for a few months out of the year so be sure to grab a pint while you can!",
+        user_id=9,
+    )
+
+    icecream20 = IceCream(
+        flavor_name="Metcha Matcha",
+        category="Vegan",
+        icecream_pic_url="https://static.toiimg.com/thumb/57810182.cms?imgsize=1796105&width=800&height=800",
+        description="The slight bitterness and strong flavors of Matcha balanced with the sweetness of cream is truly a heavenly combination that you will instantly fall in love with. ",
+        user_id=10,
     )
 
     db.session.add(funky_monkey)
@@ -156,6 +180,10 @@ def seed_icecreams():
             icecream14,
             icecream15,
             icecream16,
+            icecream17,
+            icecream18,
+            icecream19,
+            icecream20,
         ]
     )
 
