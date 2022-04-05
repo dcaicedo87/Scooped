046cfa5771b64ce416510755ae492348d3ac192b
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-04-05 03:48:30 +0000

    adding styling to nav bar and image with logo

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,46 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
+import scoopedLogo from './img/scooped_logo.png'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <div className='navbar-container'>
+      <div className='navbar-img-container'>
+        <Link to='/'>
+          <img src={scoopedLogo} width='80px'></img>
+        </Link>
+      </div>
+      <nav>
+        <ul className='navbar-container-wrapper'>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
