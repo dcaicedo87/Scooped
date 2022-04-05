8dab60244c6fb24ba2b372335651c956b645401d
Junki Sato <j9sato11@gmail.com>
2022-04-05 17:22:13 -0600

    profile update

--- react-app/src/components/EditProfile/index.js
+++ react-app/src/components/EditProfile/index.js
@@ -1,82 +1,96 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from "react-router-dom";
-import { updateProfile } from '../../store/profile';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { updateProfile } from "../../store/profile";
+import { authenticate } from "../../store/session";
 
 function EditProfile() {
-    const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
-    const userId = sessionUser.id;
-    const curUserName = sessionUser.username;
-    const userEmail = sessionUser.email;
-    const [userName, setUserName] = useState(curUserName)
-    const [email, setEmail] = useState(userEmail)
-    const [errors, setErrors] = useState([])
-    const history = useHistory()
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const userId = sessionUser.id;
+  const curUserName = sessionUser.username;
+  const userEmail = sessionUser.email;
+  const [username, setUsername] = useState(curUserName);
+  const [email, setEmail] = useState(userEmail);
+  const [errors, setErrors] = useState([]);
+  const history = useHistory();
 
-    const updateUserName = (e) => setUserName(e.target.value)
-    const updateUserEmail = (e) => setEmail(e.target.value)
+  const updateUserName = e => setUsername(e.target.value);
+  const updateUserEmail = e => setEmail(e.target.value);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    console.log("useEffect username", curUserName === username);
+    console.log("useEffect email", userEmail === email);
+  }, [dispatch, username, email]);
 
-        const updatedProfile = {
-            id: userId,
-            userName,
-            email,
-        }
+  const handleSubmit = async e => {
+    e.preventDefault();
 
-        //error validators
-        setErrors([]);
+    const updatedProfile = {
+      id: userId,
+      username,
+      email,
+    };
 
-        const newErrors = []
+    //error validators
+    setErrors([]);
 
-        if (updatedProfile.userName.length < 4) {
-            newErrors.push("Username must be 4 characters or more!")
-        }
-        if (updatedProfile.email.length < 4) {
-            newErrors.push("Email must be 4 characters or more!")
-        }
+    const newErrors = [];
 
-        if (newErrors.length > 0) {
-            setErrors(newErrors);
-            return;
-        }
-
-        dispatch(updateProfile(updatedProfile))
-        history.push(`/users/${userId}`)
+    if (updatedProfile.username.length < 4) {
+      newErrors.push("Username must be 4 characters or more!");
+    }
+    if (updatedProfile.email.length < 4) {
+      newErrors.push("Email must be 4 characters or more!");
     }
 
-
-    const handleCancel = async (e) => {
-        e.preventDefault();
-        history.push(`/users/${userId}`)
+    if (newErrors.length > 0) {
+      setErrors(newErrors);
+      return;
     }
 
-    return (
-        <section>
-            <div>
-                <ul>
-                    {errors.map((err) => (
-                        <li key={err}>{err}</li>
-                    ))}
-                </ul>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <input type='hidden' value={userId} />
-                <label>Username</label>
-                <input type="text" placeholder="Username..." value={userName} onChange={updateUserName} />
-                <label>Email</label>
-                <input type="email" placeholder="New Email..." value={email} onChange={updateUserEmail}/>
-                <button type="Submit">
-                    Accept
-                </button>
-                <button type="button" onClick={handleCancel}>
-                    Cancel
-                </button>
-            </form>
-        </section>
-    )
+    await dispatch(updateProfile(updatedProfile));
+    await dispatch(authenticate());
+    await history.push(`/users/${userId}`);
+  };
+
+  const handleCancel = async e => {
+    e.preventDefault();
+    history.push(`/users/${userId}`);
+  };
+
+  return (
+    <section>
+      <div>
+        <ul>
+          {errors.map(err => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" value={userId} />
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Username..."
+          value={username}
+          onChange={updateUserName}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="New Email..."
+          value={email}
+          onChange={updateUserEmail}
+        />
+        <button type="Submit">Accept</button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default EditProfile;
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,51 +1,46 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import LoggedNavComponent from "./LoggedNavComponent";
+import NonLoggedNavComponent from "./NonLoggedNavComponent";
+import "./NavBar.css";
+import scoopedLogo from "./img/scooped_logo.png";
+import { useSelector } from "react-redux";
 
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
-import './NavBar.css'
-import scoopedLogo from './img/scooped_logo.png'
-import { useSelector } from 'react-redux';
-
-const NavBar = () => {
-  const sessionUser = useSelector((state) => state.session.user);
+const NavBar = ({ loaded }) => {
+  const sessionUser = useSelector(state => state.session.user);
   const userId = sessionUser?.id;
 
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <LoggedNavComponent userId={userId} />;
+  } else {
+    sessionLinks = <NonLoggedNavComponent />;
+  }
+
   return (
-    <div className='navbar-container'>
-      <div className='navbar-img-container'>
-        <Link to='/'>
-          <img src={scoopedLogo} alt="" width='80px'></img>
+    <div className="navbar-container">
+      <div className="navbar-img-container">
+        <Link to="/">
+          <img src={scoopedLogo} alt="" width="80px"></img>
         </Link>
       </div>
       <nav>
-        <ul className='navbar-container-wrapper'>
+        <ul className="navbar-container-wrapper">
           <li>
-            <NavLink to='/' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
+            <NavLink
+              to="/"
+              exact={true}
+              activeClassName="active"
+              style={{ textDecoration: "none" }}
+            >
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink to='/login' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/sign-up' exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
-              Sign Up
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={`/users/${userId}`} exact={true} activeClassName='active' style={{ textDecoration: 'none' }}>
-              My Profile
-            </NavLink>
-          </li>
-          <li>
-            <LogoutButton />
-          </li>
+          {loaded && sessionLinks}
         </ul>
       </nav>
     </div>
   );
-}
+};
 
 export default NavBar;
