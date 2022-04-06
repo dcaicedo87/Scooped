5907e7a69859e4214b80a3b19bb7b452fbfea9a0
Junki Sato <j9sato11@gmail.com>
2022-04-06 14:24:42 -0600

    fixed warnings

--- react-app/src/components/IceCreamPage/index.js
+++ react-app/src/components/IceCreamPage/index.js
@@ -7,9 +7,8 @@ import { getReviewsThunk } from "../../store/review";
 import { getAllIceCreamsThunk } from "../../store/icecream";
 import AddReviewModal from "../ModalReviewPost";
 
-import './IceCreamPage.css'
-import { getUsersThunk } from '../../store/user';
-
+import "./IceCreamPage.css";
+import { getUsersThunk } from "../../store/user";
 
 const IceCreamPage = () => {
   const dispatch = useDispatch();
@@ -21,19 +20,15 @@ const IceCreamPage = () => {
     Object.values(state.review).reverse()
   );
 
-  let sum = 0
+  let sum = 0;
 
-  for (let i = 0; i < reviewList.length; i++){
-    sum += reviewList[i].rating
+  for (let i = 0; i < reviewList.length; i++) {
+    sum += reviewList[i].rating;
   }
 
-  const avgRating = sum/reviewList.length
+  const avgRating = sum / reviewList.length;
 
-  const reviewObj = useSelector(state => state.review)
-
-  const userObj = useSelector(state => state.user)
-
-  const userList = Object.values(userObj)
+  const userObj = useSelector(state => state.user);
 
   useEffect(() => {
     dispatch(getAllIceCreamsThunk());
@@ -44,37 +39,45 @@ const IceCreamPage = () => {
   }, [dispatch, iceCreamId]);
 
   useEffect(() => {
-    dispatch(getUsersThunk())
-  }, [dispatch])
-
+    dispatch(getUsersThunk());
+  }, [dispatch]);
 
   return (
     <div className="body-iceCreamPage">
       {currentIceCream && (
         <div>
-
-          <div className='main-content-iceCreamPage'>
-            <div className='big-image-container'>
-              <img src={currentIceCream.icecream_pic_url} alt="IceCream" key={currentIceCream.id} />
+          <div className="main-content-iceCreamPage">
+            <div className="big-image-container">
+              <img
+                src={currentIceCream.icecream_pic_url}
+                alt="IceCream"
+                key={currentIceCream.id}
+              />
             </div>
-            <h2 key={currentIceCream.id + "C"}>{currentIceCream.flavor_name}</h2>
-            <div className='description-iceCreamPage'>
-              <p className='paragraph-iceCreamPage' key={currentIceCream.id + "K"}>{currentIceCream.description}</p>
-
+            <h2 key={currentIceCream.id + "C"}>
+              {currentIceCream.flavor_name}
+            </h2>
+            <div className="description-iceCreamPage">
+              <p
+                className="paragraph-iceCreamPage"
+                key={currentIceCream.id + "K"}
+              >
+                {currentIceCream.description}
+              </p>
             </div>
           </div>
           <div>
             <h2>Rating: {`${avgRating}/5`}</h2>
             <h3>Reviews</h3>
             <AddReviewModal />
-            {reviewList.length === 0 ? <p>At this moment there are no Reviewsfor this IceCream</p> : null}
-            {
-            reviewList?.map(({ id, content, rating, user_id }) => (
-
+            {reviewList.length === 0 ? (
+              <p>At this moment there are no Reviewsfor this IceCream</p>
+            ) : null}
+            {reviewList?.map(({ id, content, rating, user_id }) => (
               <div>
-                <li key={id + 'O' }>{userObj[user_id].username}</li>
-                <li key={id + 'A'}>{content}</li>
-                <li key={id + 'B'}>{`${rating}/5`}</li>
+                <li key={id + "O"}>{userObj[user_id].username}</li>
+                <li key={id + "A"}>{content}</li>
+                <li key={id + "B"}>{`${rating}/5`}</li>
               </div>
             ))}
           </div>
