78a54f27cc9c09ca8c23d8c021f4d3aa4d52bf1b
Junki Sato <j9sato11@gmail.com>
2022-04-06 14:03:40 -0600

    created POST /reviews/:id route

--- react-app/src/components/IceCreamPage/index.js
+++ react-app/src/components/IceCreamPage/index.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getReviewsThunk } from '../../store/review';
+import { getReviewsThunk } from "../../store/review";
 
 import { getAllIceCreamsThunk } from "../../store/icecream";
+import AddReviewModal from "../ModalReviewPost";
 
-import './IceCreamPage.css'
-
+import "./IceCreamPage.css";
 
 const IceCreamPage = () => {
-
   const dispatch = useDispatch();
-  let iceCreamId = useParams().iceCreamId
-  const iceCreamObject = useSelector(state => state.iceCream)
-  const currentIceCream = iceCreamObject[iceCreamId]
+  let iceCreamId = useParams().iceCreamId;
+  const iceCreamObject = useSelector(state => state.iceCream);
+  const currentIceCream = iceCreamObject[iceCreamId];
 
-  const reviewList = useSelector(state => Object.values(state.review).reverse())
+  const reviewList = useSelector(state =>
+    Object.values(state.review).reverse()
+  );
 
   useEffect(() => {
     dispatch(getAllIceCreamsThunk());
@@ -26,38 +27,36 @@ const IceCreamPage = () => {
     dispatch(getReviewsThunk(iceCreamId));
   }, [dispatch]);
 
-
-
-
-
-
-
   return (
-    <div className='body-iceCreamPage'>
-      {currentIceCream &&
+    <div className="body-iceCreamPage">
+      {currentIceCream && (
         <div>
-          <div className='main-content-iceCreamPage'>
-            <div className='big-image-container'>
+          <div className="main-content-iceCreamPage">
+            <div className="big-image-container">
               <img src={currentIceCream.icecream_pic_url} alt="IceCream" />
             </div>
             <h2>{currentIceCream.flavor_name}</h2>
-            <div className='description-iceCreamPage'>
-              <p className='paragraph-iceCreamPage'>{currentIceCream.description}</p>
+            <div className="description-iceCreamPage">
+              <p className="paragraph-iceCreamPage">
+                {currentIceCream.description}
+              </p>
             </div>
           </div>
           <div>
             <h3>Reviews</h3>
-            {reviewList.length === 0 ? <p>At this moment there are no Reviewsfor this IceCream</p> : null}
+            <AddReviewModal />
+            {reviewList.length === 0 ? (
+              <p>At this moment there are no Reviews for this IceCream</p>
+            ) : null}
             {reviewList?.map(({ id, content, rating }) => (
               <div>
                 <p key={id}>{content}</p>
                 <p key={id}>{rating}</p>
               </div>
-            ))
-            }
+            ))}
           </div>
         </div>
-      }
+      )}
     </div>
   );
 };
