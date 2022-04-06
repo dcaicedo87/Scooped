ceb1f4149b1c244455490a1facffa91200203780
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-04-06 19:22:32 -0400

    addev validations to create ice cream

--- react-app/src/components/ModalAdd/AddIceCream.js
+++ react-app/src/components/ModalAdd/AddIceCream.js
@@ -15,6 +15,7 @@ function AddIceCreamForm() {
     const [category, setCategory] = useState("Dairy");
     const [iceCreamUrl, setIceCreamUrl] = useState("");
     const [description, setDescription] = useState("");
+    const [errors, setErrors] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,54 +26,81 @@ function AddIceCreamForm() {
             description,
             user_id: sessionUser.id
         }
+
+        //error validation
+        setErrors([])
+
+        const newErrors = [];
+
+        if (iceCream.flavor_name.length < 4) {
+            newErrors.push("IceCream's name must be 4 characters or more")
+        }
+
+        if (iceCream.description.length < 4) {
+            newErrors.push("IceCream's description must be 4 characters or more")
+        }
+
+        if (newErrors.length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+
         dispatch(addIceCreamThunk(iceCream))
         window.location.reload(false)
         history.push('/');
-
     }
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                IceCream Name
-                <input
-                    type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    required />
-            </label>
-            <label>
-                Category
-                <select
-                    value={category}
-                    onChange={e => setCategory(e.target.value)}
-                    required
-                >
-                    <option value="dairy">Dairy</option>
-                    <option value="vegan">Vegan</option>
-                    <option value="gelato">Gelato</option>
-                    <option value="super-market">SuperMarket</option>
-                </select>
-            </label>
-            <label>
-                Picture
-                <input
-                    type="text"
-                    value={iceCreamUrl}
-                    onChange={e => setIceCreamUrl(e.target.value)}
-                    required />
-            </label>
-            <label>
-                Description
-                <input
-                    type="text"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                    required />
-            </label>
-            <button type="submit">Add IceCream</button>
-        </form>
+        <div>
+            <div>
+            <ul className="post-icecream-errors">
+                {errors.map((err) => (
+                    <li key={err}>{err}</li>
+                 ))}
+            </ul>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    IceCream Name
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required />
+                </label>
+                <label>
+                    Category
+                    <select
+                        value={category}
+                        onChange={e => setCategory(e.target.value)}
+                        required
+                    >
+                        <option value="dairy">Dairy</option>
+                        <option value="vegan">Vegan</option>
+                        <option value="gelato">Gelato</option>
+                        <option value="super-market">SuperMarket</option>
+                    </select>
+                </label>
+                <label>
+                    Picture
+                    <input
+                        type="text"
+                        value={iceCreamUrl}
+                        onChange={e => setIceCreamUrl(e.target.value)}
+                        required />
+                </label>
+                <label>
+                    Description
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                        required />
+                </label>
+                <button type="submit">Add IceCream</button>
+            </form>
+        </div>
     )
 }
 
--- react-app/src/components/ModalReviewPost/AddReview.js
+++ react-app/src/components/ModalReviewPost/AddReview.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { addReviewThunk } from "../../store/review";
 import "../HomePage/homepage.css";
@@ -9,11 +9,11 @@ function AddReview() {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
 
-  const history = useHistory();
   const ice_cream_id = useParams().iceCreamId;
 
   const [content, setContent] = useState("");
   const [rating, setRating] = useState(0);
+  const [errors, setErrors] = useState([])
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -23,38 +23,62 @@ function AddReview() {
       user_id: sessionUser.id,
       ice_cream_id,
     };
+
+    //error validation
+    setErrors([])
+
+    const newErrors = [];
+
+    if (review.content.length < 4) {
+      newErrors.push("Content must be 4 characters or more.");
+    }
+
+    if (newErrors.length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
     dispatch(addReviewThunk(review));
     window.location.reload(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Review Content
-        <input
-          type="text"
-          value={content}
-          onChange={e => setContent(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Rating
-        <select
-          value={rating}
-          onChange={e => setRating(e.target.value)}
-          required
-        >
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-      </label>
-      <button type="submit">Add Review</button>
-    </form>
+    <div>
+      <div>
+        <ul className="post-review-errors">
+            {errors.map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Review Content
+          <input
+            type="text"
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Rating
+          <select
+            value={rating}
+            onChange={e => setRating(e.target.value)}
+            required
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <button type="submit">Add Review</button>
+      </form>
+    </div>
   );
 }
 
