369478539a0d556c69d4dbd654083294bd2d2a5a
Leon Troper <ltroper@gmail.com>
2022-04-07 00:33:55 -0600

    worked on a lot of css

--- react-app/src/components/ModalReviewEdit/index.js
+++ react-app/src/components/ModalReviewEdit/index.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import EditReview from "./EditReview";
 
-import "../HomePage/homepage.css";
-
 function EditReviewModal({review}) {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className="big-button" onClick={() => setShowModal(true)}>
-        Edit review
+      <button onClick={() => setShowModal(true)}>
+        Edit 
       </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
